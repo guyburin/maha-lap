51a47fc220b72e5a6b8985591b71b7d9f53954b1
guyburin <guyfreedom02@gmail.com>
2019-09-22 11:59:27 -0700

    add DayOrNight

--- maha-lap.js
+++ maha-lap.js
@@ -1,52 +1,72 @@
 class MahaLap {
-  static tamnaiWithDay(day) {
-    switch (day) {
-      case "อาทิตย์":
-        return 1;
-      case "จันทร์":
-        return 2;
-      case "อังคาร":
-        return 3;
-      case "พุธ":
-        return 4;
-      case "พฤหัสบดี":
-        return 5;
-      case "ศุกร์":
-        return 6;
-      case "เสาร์":
-        return 7;
-      case "สงกรานต์":
-        return 13;
-      case "ฮาโลวีน":
-        return 31;
-      case "คริสมาสต์":
-        return 25;
-      default:
-        return 99;
+    static tamnaiWithDay(day) {
+        switch (day) {
+            case "อาทิตย์":
+                return 1;
+            case "จันทร์":
+                return 2;
+            case "อังคาร":
+                return 3;
+            case "พุธ":
+                return 4;
+            case "พฤหัสบดี":
+                return 5;
+            case "ศุกร์":
+                return 6;
+            case "เสาร์":
+                return 7;
+            case "สงกรานต์":
+                return 13;
+            case "ฮาโลวีน":
+                return 31;
+            case "คริสมาสต์":
+                return 25;
+            default:
+                return 99;
+        }
     }
-  }
 
-  static tamnaiWithStory(Story) {
-    const length = Story.length;
-    let sum = 0;
-    for (var i = 1; i <= length; i++) {
-      sum += i;
+    static tamnaiWithStory(Story) {
+        const length = Story.length;
+        let sum = 0;
+        for (var i = 1; i <= length; i++) {
+            sum += i;
+        }
+        switch (sum) {
+            case 3:
+                return sum;
+            case 6:
+                return sum;
+            case 10:
+                return sum;
+            case 105:
+                return sum;
+            case 21:
+                return sum;
+            case 55:
+                return sum;
+            default:
+                return 99;
+        }
     }
-    switch (sum) {
-      case 3:
-        return sum;
-      case 6:
-        return sum;
-      case 10:
-        return sum;
-      case 105:
-        return sum;
-      case 21:
-        return sum;
-      default:
-        return 99;
+
+    static tamnaiWithTime(time) {
+        var random = Math.random(); // return random;
+        if (time == "กลางวัน") {
+            if (random >= 0.5) {
+                return random;
+            } else {
+                return random = 0.5;
+            }
+        } else if (time == "กลางคืน") {
+            if (random <= 0.5) {
+                return random;
+            } else {
+                return random = 0.5;
+            }
+        }
+
     }
-  }
 }
 
 module.exports = MahaLap;
--- maha-lap.test.js
+++ maha-lap.test.js
@@ -1,68 +1,92 @@
 const MahaLap = require("./maha-lap");
 
 describe("Test Maha-Lap", () => {
-  test("Test Tamnai with day: monday", () => {
-    const result = MahaLap.tamnaiWithDay("จันทร์");
-    expect(result).toEqual(2);
-  });
-  test("Test Tamnai with day: sunday", () => {
-    const result = MahaLap.tamnaiWithDay("อาทิตย์");
-    expect(result).toEqual(1);
-  });
-  test("Test Tamnai with day: tuesday", () => {
-    const result = MahaLap.tamnaiWithDay("อังคาร");
-    expect(result).toEqual(3);
-  });
-  test("Test Tamnai with day Wednesday ", () => {
-    const result = MahaLap.tamnaiWithDay("พุธ");
-    expect(result).toEqual(4);
-  });
-  test("Test Tamnai with day: Thursday ", () => {
-    const result = MahaLap.tamnaiWithDay("พฤหัสบดี");
-    expect(result).toEqual(5);
-  });
-  test("Test Tamnai with day: Friday ", () => {
-    const result = MahaLap.tamnaiWithDay("ศุกร์");
-    expect(result).toEqual(6);
-  });
-  test("Test Tamnai with day: Saturay ", () => {
-    const result = MahaLap.tamnaiWithDay("เสาร์");
-    expect(result).toEqual(7);
-  });
-  test("Test Tamnai with day: Songkran", () => {
-    const result = MahaLap.tamnaiWithDay("สงกรานต์");
-    expect(result).toEqual(13);
-  });
-  test("Test Tamnai with day: Halloween", () => {
-    const result = MahaLap.tamnaiWithDay("ฮาโลวีน");
-    expect(result).toEqual(31);
-  });
-  test("Test Tamnai with day: Christmas", () => {
-    const result = MahaLap.tamnaiWithDay("คริสมาสต์");
-    expect(result).toEqual(25);
-  });
-  test("Test Tamnai with day: Not have a day", () => {
-    const result = MahaLap.tamnaiWithDay("Not have a day");
-    expect(result).toEqual(99);
-  });
-  test("Test Tamnai with story: Snack", () => {
-    const result = MahaLap.tamnaiWithStory("งู");
-    expect(result).toEqual(3);
-  });
-  test("Test Tamnai with story: fish", () => {
-    const result = MahaLap.tamnaiWithStory("ปลา");
-    expect(result).toEqual(6);
-  });
-  test("Test Tamnai with story: buffalo", () => {
-    const result = MahaLap.tamnaiWithStory("ควาย");
-    expect(result).toEqual(10);
-  });
-  test("Test Tamnai with story: green snake", () => {
-    const result = MahaLap.tamnaiWithStory("งูเขียวหางไหม้");
-    expect(result).toEqual(105);
-  });
-  test("Test Tamnai with story: panda", () => {
-    const result = MahaLap.tamnaiWithStory("แพนด้า");
-    expect(result).toEqual(21);
-  });
+    test("Test Tamnai with day: monday", () => {
+        const result = MahaLap.tamnaiWithDay("จันทร์");
+        expect(result).toEqual(2);
+    });
+    test("Test Tamnai with day: sunday", () => {
+        const result = MahaLap.tamnaiWithDay("อาทิตย์");
+        expect(result).toEqual(1);
+    });
+    test("Test Tamnai with day: tuesday", () => {
+        const result = MahaLap.tamnaiWithDay("อังคาร");
+        expect(result).toEqual(3);
+    });
+    test("Test Tamnai with day Wednesday ", () => {
+        const result = MahaLap.tamnaiWithDay("พุธ");
+        expect(result).toEqual(4);
+    });
+    test("Test Tamnai with day: Thursday ", () => {
+        const result = MahaLap.tamnaiWithDay("พฤหัสบดี");
+        expect(result).toEqual(5);
+    });
+    test("Test Tamnai with day: Friday ", () => {
+        const result = MahaLap.tamnaiWithDay("ศุกร์");
+        expect(result).toEqual(6);
+    });
+    test("Test Tamnai with day: Saturay ", () => {
+        const result = MahaLap.tamnaiWithDay("เสาร์");
+        expect(result).toEqual(7);
+    });
+    test("Test Tamnai with day: Songkran", () => {
+        const result = MahaLap.tamnaiWithDay("สงกรานต์");
+        expect(result).toEqual(13);
+    });
+    test("Test Tamnai with day: Halloween", () => {
+        const result = MahaLap.tamnaiWithDay("ฮาโลวีน");
+        expect(result).toEqual(31);
+    });
+    test("Test Tamnai with day: Christmas", () => {
+        const result = MahaLap.tamnaiWithDay("คริสมาสต์");
+        expect(result).toEqual(25);
+    });
+    test("Test Tamnai with day: Not have a day", () => {
+        const result = MahaLap.tamnaiWithDay("Not have a day");
+        expect(result).toEqual(99);
+    });
+    test("Test Tamnai with story: Snack", () => {
+        const result = MahaLap.tamnaiWithStory("งู");
+        expect(result).toEqual(3);
+    });
+    test("Test Tamnai with story: fish", () => {
+        const result = MahaLap.tamnaiWithStory("ปลา");
+        expect(result).toEqual(6);
+    });
+    test("Test Tamnai with story: buffalo", () => {
+        const result = MahaLap.tamnaiWithStory("ควาย");
+        expect(result).toEqual(10);
+    });
+    test("Test Tamnai with story: green snake", () => {
+        const result = MahaLap.tamnaiWithStory("งูเขียวหางไหม้");
+        expect(result).toEqual(105);
+    });
+    test("Test Tamnai with story: panda", () => {
+        const result = MahaLap.tamnaiWithStory("แพนด้า");
+        expect(result).toEqual(21);
+    });
+    test("Test Tamnai with story: Cinderella", () => {
+        const result = MahaLap.tamnaiWithStory("Cinderella");
+        expect(result).toEqual(55);
+    });
+    test("Test Tamnai with Time:กลางวัน random= 0.98 return 0.98", () => {
+        global.Math.random = jest.fn(() => 0.98);
+        const result = MahaLap.tamnaiWithTime("กลางวัน");
+        expect(result).toEqual(0.98);
+    });
+    test("Test Tamnai with Time:กลางวัน random= 0.4 return 0.5", () => {
+        global.Math.random = jest.fn(() => 0.4);
+        const result = MahaLap.tamnaiWithTime("กลางวัน");
+        expect(result).toEqual(0.5);
+    });
+    test("Test Tamnai with Time:กลางวัน random= 0.4 return 0.4", () => {
+        global.Math.random = jest.fn(() => 0.4);
+        const result = MahaLap.tamnaiWithTime("กลางคืน");
+        expect(result).toEqual(0.4);
+    });
+    test("Test Tamnai with Time:กลางวัน random= 0.64 return 0.5", () => {
+        global.Math.random = jest.fn(() => 0.64);
+        const result = MahaLap.tamnaiWithTime("กลางคืน");
+        expect(result).toEqual(0.5);
+    });
 });
