85c9b1229a842ab5b15aae96ae55cbb53bb73534
guyburin <guyfreedom02@gmail.com>
2019-09-22 10:30:51 -0700

    add day 1-7

--- maha-lap.js
+++ maha-lap.js
@@ -7,6 +7,21 @@ class MahaLap {
       case "จันทร์":
         return 2;
         break;
+      case "อังคาร":
+        return 2;
+        break;
+      case "พุธ":
+        return 2;
+        break;
+      case "พฤหัสบดี":
+        return 2;
+        break;
+      case "ศุกร์":
+        return 2;
+        break;
+      case "เสาร์":
+        return 2;
+        break;
     }
   }
 }
--- maha-lap.test.js
+++ maha-lap.test.js
@@ -9,4 +9,24 @@ describe("Test Maha-Lap", () => {
     const result = MahaLap.tamnaiWithDay("อาทิตย์");
     expect(result).toEqual(1);
   });
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("อังคาร");
+    expect(result).toEqual(3);
+  });
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("พุธ");
+    expect(result).toEqual(4);
+  });
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("พฤหัสบดี");
+    expect(result).toEqual(5);
+  });
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("ศุกร์");
+    expect(result).toEqual(6);
+  });
+  test("Test Tamnai with day", () => {
+    const result = MahaLap.tamnaiWithDay("เสาร์");
+    expect(result).toEqual(7);
+  });
 });
